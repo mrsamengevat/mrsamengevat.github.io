8677748e60b87319d61bebdba361f6698df6aefb
mrsamengevat <spraakwater2020@gmail.com>
2023-08-20 18:09:26 +0200

    Site updated: 2023-08-20 18:09:26

--- js/search.js
+++ js/search.js
@@ -135,3 +135,14 @@ var getSearchFile = function(){
     var path = "/search.xml";
     searchFunc(path, 'search-input', 'search-result');
 }
+
+
+.search-overlay {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  backdrop-filter: blur(10px);
+}
